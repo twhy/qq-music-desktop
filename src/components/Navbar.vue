7b61042f81c048d3cf0c4135462316e8cce677a9
Tom Wang <tom.wanghaiyi@gmail.com>
2017-09-29 16:40:13 +0800

    Update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="navbar">
-    Nabbar
+    <div class="refresh">
+
+    </div>
   </div>
 </template>
 
@@ -10,10 +12,22 @@ export default {
 }
 </script>
 
-
 <style lang="scss" scoped>
   .navbar {
     height: 46px;
     border-bottom: 1px solid #efefef;
+    display: flex;
+    align-items: center;
+  }
+  .refresh {
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    margin-left: 5px;
+    background: url(../assets/images/icon-refresh.png);
+    background-size: 20px;
+    &:hover {
+      background-image: url(../assets/images/icon-refresh-hover.png);
+    }
   }
 </style>
--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,16 +1,39 @@
 <template>
-  <div>精选</div>
+  <div class="recommend">推荐</div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      slides: [
+        { image: "http://y.gtimg.cn/music/common/upload/t_focus_info_iphone/148078.jpg" },
+        { image: "http://y.gtimg.cn/music/common/upload/t_focus_info_iphone/148163.jpg" },
+        { image: "http://y.gtimg.cn/music/common/upload/t_focus_info_iphone/148077.jpg" },
+        { image: "http://y.gtimg.cn/music/common/upload/t_focus_info_iphone/147822.jpg" },
+        { image: "http://y.gtimg.cn/music/common/upload/t_focus_info_iphone/148096.jpg" }
+      ]
+    }
+  },
   created() {
     console.log('created', this.$root.$data.state)
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
+  .el-carousel__item--card {
+    width: auto;
+  }
+</style>
 
+
+<style lang="scss" scoped>
+  .slice {
+    .image {
+      width: 525px;
+      height: 210px;
+    }
+  }
 </style>
 
